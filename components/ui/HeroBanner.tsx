02e29a38b99e2491fd76c47bd123c89bd0b385b9
Arvindeva Wibisono <arvindeva@gmail.com>
2024-01-22 18:14:33 +0100

    date picker

--- components/ui/HeroBanner.tsx
+++ components/ui/HeroBanner.tsx
@@ -9,19 +9,23 @@ export default function HeroBanner() {
   return (
     <div className="px-6 pt-14 container max-w-screen-2xl lg:px-8">
       <div className="text-center">
-        <h1 className="text-4xl font-bold tracking-tight text-neutral-200 sm:text-6xl">
-          Book your stay at .titik.tiga.
+        <h1 className="text-9xl font-bold tracking-tight text-neutral-200 sm:text-9xl mb-10">
+          ...
         </h1>
-        <p className="mt-6 text-lg leading-8 text-neutral-200 max-w-screen-md m-auto">
+        <h1 className="text-4xl font-bold tracking-tight text-neutral-200 sm:text-6xl mb-10">
+          book your stay at .titik.tiga.
+        </h1>
+        <p className="mt-6 text-lg leading-8 text-neutral-500 max-w-screen-md m-auto">
           Immerse yourself in the allure of Bali and create unforgettable
-          memories with .titik.tiga. Impeccably designed rooms and villas offer
-          you the utmost comfort and privacy, where every detail is meticulously
-          curated to pamper your senses.
+          memories with .titik.tiga. Our impeccably designed rooms offer you the
+          utmost comfort and privacy, where every detail is meticulously curated
+          to pamper your senses.
         </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
+        <div className="mt-10 flex items-center justify-center gap-x-2">
           <DatePickerWithRange />
+
           <div>
-            <Button>Check Availability!</Button>
+            <Button>Check Availability</Button>
           </div>
         </div>
       </div>
